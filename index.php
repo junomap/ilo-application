b71cbe35ebdb0ad6a10121d32ba58bea639bf417
Ilo_PULULU <ilopululu73@gmail.com>
2021-01-01 17:34:34 +0100

    Creation des dossiers image, models, view, et ajout des fichiers upn_kinhsasa_blason.png, mod1.php et vue1.php pour premiere essaie et experimentation

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-je suis une app
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My TFC web application</title>
+	<meta charset="utf-8">
+	<style type="text/css" ></style>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+ 
+</body>
+</html>
